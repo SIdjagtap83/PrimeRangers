4d84d371767769fab1c3b1bbb8d2c70bae3fb49f
Siddhant Jagtap <64470172+SIdjagtap83@users.noreply.github.com>
2020-10-10 15:10:06 +0530

    Update2

--- PrimeRangers.java
+++ PrimeRangers.java
@@ -1,101 +1,157 @@
 import java.util.*;
+import java.lang.Math;
 
 class PrimeRangers
 {
-
+  
      public static void main(String args[])
-       {
-                int no_2,no_3,no_5,no_7,count,temp;
-                
+      {        
+         
+               int M = 1000000007;
+               int range = (int)Math.pow(10, 18);
+
                 Scanner sc = new Scanner(System.in);
+                // Scan Number Of Inputs
                 int n=sc.nextInt();
                 sc.nextLine();
+         if(n<1||n>5)
+         {
+            System.out.println("Input Out Of Range");
+         }
+         else
+         {  
+                // Objects created (size same as that of the inputs)
+                OperationOnPR[] obj = new OperationOnPR[n];
+
                 for(int i=0;i<n;i++)
                 {
                      int fn = sc.nextInt();
                      int ln = sc.nextInt();
-                     sc.nextLine();
-                     count=0;
-                     
-                     
 
-                     for(int F=fn;F<=ln;F++)
+                     if(fn<1||fn>ln||ln>range)
                      {
-                           no_2=0;
-                           no_3=0;
-                           no_5=0;
-                           no_7=0;
-                           
+                        System.out.println("Input Out Of Range");
+                        
+                     }
+                     else
+                     {
+                         // object initialization 
+                          obj[i] = new OperationOnPR(fn, ln);
+                          sc.nextLine();   
+                    } 
+                }
+                sc.close();
 
+                for(int i=0;i<n;i++)
+                {
+                  System.out.println(obj[i].numberOfPrimeRangers()%M);
+                }
 
-                         if(F/10==0)
-                           {
-                             if(F==2)
-                                 {
-                                 no_2+=1;
-                                 }
-                              if(F==3)
-                                 {
-                                 no_3+=1;
-                                 }
-                              if(F==5)
-                                 {
-                                 no_5+=1;
-                                 }
-                              if(F==7)
-                                 {
-                                 no_7+=1;
-                                 }   
+         }
+                
+      }
+       
+       
 
-                           }
-                         else
-                           {
-                               int T = F;
+}
 
-                             while(T>0)
-                             {
-                              temp = T%10;
-                              T = T/10;
-                                 if(temp==2)
-                                    {
-                                     no_2+=1;
-                                    }
-                                 else if(temp==3)
-                                    {
-                                     no_3+=1;
-                                    }
-                                 else if(temp==5)
-                                    {
-                                     no_5+=1;
-                                    }
-                                 else if(temp==7)
-                                    {
-                                     no_7+=1;
-                                    }   
-                                 else{}
-                              
-                                 
+class OperationOnPR
+{
+    private int first_num,last_num;
+    private int no_2,no_3,no_5,no_7,count,temp;
 
-                              }
+    OperationOnPR(int first_num,int last_num){
 
-                           }
+        this.first_num = first_num;
+        this.last_num = last_num;
 
-                             
-                            if(no_2>=no_3&&no_3>=no_5&&no_5>=no_7)
-                               {
-                                  count+=1;
-                               }     
-                             
+    }
 
+        // Getter
+        public int getFirstNum()
+         {
+            return first_num;
+        }
+
+        public int getLasttNum()
+        {
+           return last_num;
+       }
+
+         // Setter
+         public void setFirstNum(int first_num) 
+         {
+            this.first_num = first_num;
+         }
+
+         public void setLastNum(int last_num) 
+         {
+            this.last_num = last_num;
+         }
+
+        
+         // Returning number of Prime Rangers
+         public int numberOfPrimeRangers()
+         {
+            count = 0;
+            for(int F=this.first_num;F<=this.last_num;F++)
+            {
+                  no_2=0;
+                  no_3=0;
+                  no_5=0;
+                  no_7=0;
+                  
+                if(F/10==0)
+                  {
+                    countingPrimeNumbers(F);
+                  }
+                else
+                  {
+                      int T = F;
+
+                    while(T>0)
+                    {
+                     temp = T%10;
+                     T = T/10;
+                     countingPrimeNumbers(temp);  
                      }
 
-                            System.out.println(count); 
-                     
-                     
-                }
-                
+                  }
 
-                sc.close();
-       }
+                    
+                   if(!(no_2>=no_3&&no_3>=no_5&&no_5>=no_7))
+                      {
+                         count+=1;
+                      }     
+                    
+
+            }
+
+            
+
+            return this.last_num - this.first_num - count + 1;
+         }
+
+
+         public void countingPrimeNumbers(int F2){
+
+                     if(F2==2)
+                        {
+                        no_2+=1;
+                        }
+                     if(F2==3)
+                        {
+                        no_3+=1;
+                        }
+                     if(F2==5)
+                        {
+                        no_5+=1;
+                        }
+                     if(F2==7)
+                        {
+                        no_7+=1;
+                        }   
+
+         }
 
 }
